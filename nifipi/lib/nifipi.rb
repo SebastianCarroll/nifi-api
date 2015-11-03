5e536196e666a7a86ada4f32d7261bf52e88ad27
Sebastian Carroll <seabass25@hotmail.com>
2015-11-03 22:29:09 +1100

    Adds method to get the version number

--- nifipi/lib/nifipi.rb
+++ nifipi/lib/nifipi.rb
@@ -1,11 +1,19 @@
 require "nifipi/version"
+require "json"
 
 module Nifipi
+  JSON_HEADER = {"Content-Type" => "application/json"}
   class Nifi
     attr_accessor :host, :port
     def initialize(host, port)
       @host = host
       @port = port
     end  
+
+    def version
+      uri = URI("http://#{@host}:#{@port}/nifi-api/controller/revision")
+      revision = JSON.parse(Net::HTTP.get_response(uri).body)
+      return revision["revision"]["version"]
+    end
   end 
 end
--- nifipi/spec/nifipi_spec.rb
+++ nifipi/spec/nifipi_spec.rb
@@ -1,15 +1,27 @@
 require 'spec_helper'
 
 describe Nifipi do
+  before(:each) do
+    @host = 'localhost'
+    @port = '8080'
+    @nifi = Nifipi::Nifi.new @host, @port
+  end
+
   it 'has a version number' do
     expect(Nifipi::VERSION).not_to be nil
   end
 
-  it 'sets hostname and port on which to access api' do
-    host = 'localhost'
-    port = '8080'
-    nifi = Nifipi::Nifi.new host, port
-    expect(host).to eq(nifi.host)
-    expect(port).to eq(nifi.port)
+  it 'sets @hostname and @port on which to access api' do
+    expect(@host).to eq(@nifi.host)
+    expect(@port).to eq(@nifi.port)
+  end
+
+  it 'gets the version' do
+    expect(@nifi.version.is_a? Numeric).to equal(true)
+  end
+
+  it 'has the correct json content headers' do
+    expected= {"Content-Type" => "application/json"}
+    expect(Nifipi::JSON_HEADER).to eq(expected)
   end
 end
